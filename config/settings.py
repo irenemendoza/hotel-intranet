253ce70c9a2a3f9db8e5f5fd3232c72e6f8a5151
Irene Mendoza <mendozagonzalez.irene@gmail.com>
2026-03-05 14:08:15 +0100

    fix: correct STATIC_URL path for production static files serving

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "static_root"
 
